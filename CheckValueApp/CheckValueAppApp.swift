0f7eea9e4d64e4b2ea6d342d51ed19d1e03c5028
Alex Rybachev <rybachev.as@gmail.com>
2021-06-13 20:23:43 +0400

    Initial Commit

---
+++ CheckValueApp/CheckValueAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  CheckValueAppApp.swift
+//  CheckValueApp
+//
+//  Created by Aleksandr Rybachev on 13.06.2021.
+//
+
+import SwiftUI
+
+@main
+struct CheckValueAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CheckValueApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  CheckValueApp
+//
+//  Created by Aleksandr Rybachev on 13.06.2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
